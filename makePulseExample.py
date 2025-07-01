84fb5df252d93b78a4761aafd2e2dfddad39bb8d
Mark Elo <64975054+taborelec@users.noreply.github.com>
2025-07-01 16:32:03 -0700

    Add files via upload

---
+++ makePulseExample.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  1 13:57:41 2025
+
+@author: marka
+"""
+import os
+import sys
+
+srcpath = os.path.realpath('SourceFiles')
+sys.path.append(srcpath)
+
+import warnings # this is for GUI warnings
+warnings.filterwarnings("ignore")
+
+import numpy as np
+from tevisainst import TEVisaInst
+
+# Downloaded waveform to Proteus must be a multiple of 64?
+
+def round_up_64(n):
+    """Round up to the nearest multiple of 64."""
+    return ((n + 63) // 64) * 64
+
+# --- Parameters ---
+sampleRateDAC = 1e9  # 1 GS/s
+segment_us = 100     # Total segment length in microseconds
+pulse_us = 10        # Pulse width in microseconds
+
+# --- Calculate lengths ---
+desired_segment_len = int(segment_us * 1e-6 * sampleRateDAC)
+segment_len = round_up_64(desired_segment_len)  # Must be multiple of 64
+
+desired_pulse_len = int(pulse_us * 1e-6 * sampleRateDAC)
+pulse_len = min(desired_pulse_len, segment_len)  # Pulse fits in segment
+
+# --- Create waveform: 10us pulse, rest zeros ---
+waveform = np.zeros(segment_len)
+# Example: 5 cycles in 10us, amplitude 0.8
+t = np.arange(pulse_len)
+waveform[:pulse_len] = 0.8 * np.ones(pulse_len)
+
+# --- Scale for DAC (unsigned 16-bit) ---
+max_dac = 65535
+half_dac = max_dac / 2
+segment = ((waveform + 1.0) * half_dac).astype(np.uint16)
+
+# --- Connect to Proteus and upload ---
+inst_addr = 'TCPIP::192.168.1.12::5025::SOCKET'
+inst = TEVisaInst(inst_addr)
+
+inst.send_scpi_cmd('*CLS; *RST')
+inst.send_scpi_cmd(':INST:CHAN 1')
+inst.send_scpi_cmd(f':FREQ:RAST {sampleRateDAC}')
+inst.send_scpi_cmd(':TRAC:DEL:ALL')
+inst.send_scpi_cmd(f':TRAC:DEF 1, {segment_len}')
+inst.send_scpi_cmd(':TRAC:SEL 1')
+inst.write_binary_data('*OPC?; :TRAC:DATA', segment)
+
+inst.send_scpi_cmd(':FUNC:MODE:SEGM 1')
+inst.send_scpi_cmd(':OUTP ON')
+inst.send_scpi_cmd(':INIT:CONT ON')
+
+# --- Optional: check for errors ---
+resp = inst.send_scpi_query(':SYST:ERR?')
+print("Instrument Error Status:", resp)
---
+++ makePulseGenMkrExample.py
@@ -0,0 +1,163 @@
+import os
+import sys
+
+srcpath = os.path.realpath('SourceFiles')
+sys.path.append(srcpath)
+
+from tevisainst import TEVisaInst
+
+import numpy as np
+
+#Set rates for DAC 
+sampleRateDAC = 1E9
+
+#wavefore parameters
+max_dac=(2**16)-1 # Max Dac
+half_dac=max_dac/2 # DC Level
+min_dac = 0
+data_type = np.uint16 # DAC data type
+
+# Make CH1 pulse
+onLen = 2048
+offLen = 2048
+
+dacWaveOn = np.ones(onLen)
+dacWaveOn = dacWaveOn * max_dac 
+dacWaveOn = dacWaveOn.astype(data_type)
+
+dacWaveOff = np.ones(offLen)
+dacWaveOff = dacWaveOff * min_dac 
+dacWaveOff = dacWaveOff.astype(data_type)   
+
+dacWave1 = np.concatenate([dacWaveOn, dacWaveOff])   
+ 
+
+'''
+# Connect to instrument(PXI)
+sid = 7 #PXI slot of AWT on chassis <------------ SLOT NUMBER HERE
+from teproteus import TEProteusAdmin as TepAdmin
+admin = TepAdmin() #required to control PXI module
+inst = admin.open_instrument(slot_id=sid) 
+resp = inst.send_scpi_query("*IDN?") # Get the instrument's *IDN
+print('connected to: ' + resp) # Print *IDN
+
+'''
+
+# Connect to instrument(LAN)
+inst_addr = 'TCPIP::192.168.1.12::5025::SOCKET' #Proteus Lan
+#inst_addr = 'TCPIP::127.0.0.1::5025::SOCKET' #Proteus Local 
+
+inst = TEVisaInst(inst_addr) # get instruent pointer
+resp = inst.send_scpi_query("*IDN?")
+print('Connected to: ' + resp) # print insturmrnt ID
+
+resp = inst.send_scpi_query("*OPT?")
+print('Options: ' + resp) # print insturmrnt ID
+
+# initialize DAC
+inst.send_scpi_cmd('*CLS; *RST')
+
+#AWG channel
+ch = 1 # everythinf after relates to CH 1
+cmd = ':INST:CHAN {0}'.format(ch)
+inst.send_scpi_cmd(cmd)
+
+cmd = ':FREQ:RAST {0}'.format(sampleRateDAC) 
+inst.send_scpi_cmd(cmd)
+inst.send_scpi_cmd(':INIT:CONT ON')
+inst.send_scpi_cmd(':TRAC:DEL:ALL')
+
+
+# Define segment memory
+segnum = 1
+cmd = ':TRAC:DEF {0}, {1}'.format(segnum, len(dacWave1))
+inst.send_scpi_cmd(cmd)
+
+# Select the segment
+cmd = ':TRAC:SEL {0}'.format(segnum)
+inst.send_scpi_cmd(cmd)
+
+# Increase the timeout before writing binary-data:
+inst.timeout = 30000
+# Send the binary-data with *OPC? added to the beginning of its prefix.
+inst.write_binary_data('*OPC?; :TRAC:DATA', dacWave1)
+# Set normal timeout
+inst.timeout = 10000
+
+resp = inst.send_scpi_query(':SYST:ERR?')
+print("Trace Download Error = ")
+print(resp)
+
+markerNum = 1
+mark = np.zeros(len(dacWave1) // 4, np.int8)
+mark[0:256] = 17  # 1000100 <- MArker 1 set bit 0 and 4, marker 2 = bit 1 and 5
+
+# Increase the timeout before writing binary-data:
+inst.timeout = 30000
+# Send the binary-data with *OPC? added to the beginning of its prefix.
+inst.write_binary_data('*OPC?; :MARK:DATA', mark)
+# Set normal timeout
+inst.timeout = 10000
+
+resp = inst.send_scpi_query(':SYST:ERR?')
+print("Marker Download Error = ")
+print(resp)
+
+# Select the marker to assign to above trace
+cmd = ':MARK:SEL {0}'.format(markerNum)
+inst.send_scpi_cmd(cmd)
+
+cmd = ':MARK:STAT ON'
+inst.send_scpi_cmd(cmd)
+
+resp = inst.send_scpi_query(':SYST:ERR?')
+print("Marker Error = ")
+print(resp)
+
+cmd = ':FUNC:MODE:SEGM {0}'.format(segnum)
+inst.send_scpi_cmd(cmd)
+
+cmd = ':VOLT MAX'
+rc = inst.send_scpi_cmd(cmd)
+
+cmd = ':VOLT:OFFS 0'
+rc = inst.send_scpi_cmd(cmd)
+    
+cmd = ':OUTP ON'
+rc = inst.send_scpi_cmd(cmd)
+
+resp = inst.send_scpi_query(':SYST:ERR?')
+print("End of CH1, Gen Error = ")
+print(resp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
